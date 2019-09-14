ab9ad8ffa342282c72a6745e4069fd9d363dee36
taisei <star27frog@gmail.com>
2019-09-14 17:10:06 +0900

    sato's test

--- YamanoteStationRoulette/app/src/main/java/jp/ace/pro/yamanotestationroulette/MainActivity.java
+++ YamanoteStationRoulette/app/src/main/java/jp/ace/pro/yamanotestationroulette/MainActivity.java
@@ -1,8 +1,10 @@
 package jp.ace.pro.yamanotestationroulette;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> yamanote = new ArrayList<>();
+        yamanote.add("渋谷");
+        yamanote.add("新宿");
+        yamanote.add("池袋");
+        yamanote.add("品川");
+        yamanote.add("日暮里");
+
+        String station = yamanote.get(0);
+
+        TextView textView = (TextView) findViewById(R.id.text);
+
+        textView.setText(station);
+
     }
+
+
 }
