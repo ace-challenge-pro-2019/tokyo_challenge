3a4621498302a1a038b7094ae7523a39d4b2aeb5
taisei <star27frog@gmail.com>
2019-09-23 14:36:54 +0900

    詳細ボタンを追加

--- YamanoteStationRoulette/app/src/main/java/jp/ace/pro/yamanotestationroulette/MainActivity.java
+++ YamanoteStationRoulette/app/src/main/java/jp/ace/pro/yamanotestationroulette/MainActivity.java
@@ -25,9 +25,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 駅名表示
         textView = findViewById(R.id.text);
         textView.setText(randomStation());
 
+        // 詳細ボタン
+        Button detailButton = findViewById(R.id.detailButton);
+        detailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // 駅名更新ボタン
         Button button = findViewById(R.id.updateButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(randomStation());
             }
         });
-
     }
 
     /**
