6daeacf14f3c08ec7ceb29401bd19c0daedb4ffb
SatoHiroyasu <ofx2000_acx@yahoo.co.jp>
2019-10-26 18:20:58 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	YamanoteStationRoulette/app/src/main/res/layout/activity_main.xml

---
+++ YamanoteStationRoulette/app/src/main/java/jp/ace/pro/yamanotestationroulette/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package jp.ace.pro.yamanotestationroulette;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // タイトルを非表示にします。
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // splash.xmlをViewに指定します。
+        setContentView(R.layout.activity_splash_screen);
+        Handler hdl = new Handler();
+        // 500ms遅延させてsplashHandlerを実行します。
+        hdl.postDelayed(new splashHandler(), 1000);
+    }
+    class splashHandler implements Runnable {
+        public void run() {
+            // スプラッシュ完了後に実行するActivityを指定します。
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
+            // SplashActivityを終了させます。
+            SplashScreenActivity.this.finish();
+        }
+    }
+}
