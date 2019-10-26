b71504be9a9193bafd180eb449390e57f433b10d
h.adachi <h.adachi@pro-japan.co.jp>
2019-10-26 16:24:16 +0900

    アプリからFirestoreの中身を取得するU/Iを作成。

---
+++ YamanoteStationRoulette/app/src/main/java/jp/ace/pro/yamanotestationroulette/db/FirestoreClient.java
@@ -0,0 +1,38 @@
+package jp.ace.pro.yamanotestationroulette.db;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class FirestoreClient {
+    private static final String TAG = FirestoreClient.class.getSimpleName();
+    private static final String COLLECTION_NAME = "ace-tokyo-challenge";
+
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public static void getAll() {
+        Log.d("test", "call Firestore getAll");
+        db.collection(COLLECTION_NAME)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("test", "Firestore successful!!");
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("test", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+
+}
