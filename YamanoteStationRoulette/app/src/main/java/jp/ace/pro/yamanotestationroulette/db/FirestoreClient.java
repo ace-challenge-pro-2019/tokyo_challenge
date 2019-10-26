fb32d9c18a01cca30736b996a6f99a5a037158b4
h.adachi <h.adachi@pro-japan.co.jp>
2019-10-26 16:25:38 +0900

    LOG整理。

--- YamanoteStationRoulette/app/src/main/java/jp/ace/pro/yamanotestationroulette/db/FirestoreClient.java
+++ YamanoteStationRoulette/app/src/main/java/jp/ace/pro/yamanotestationroulette/db/FirestoreClient.java
@@ -17,16 +17,14 @@ public class FirestoreClient {
     private static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public static void getAll() {
-        Log.d("test", "call Firestore getAll");
         db.collection(COLLECTION_NAME)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            Log.d("test", "Firestore successful!!");
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("test", document.getId() + " => " + document.getData());
+                                Log.d(TAG, document.getId() + " => " + document.getData());
                             }
                         } else {
                             Log.w(TAG, "Error getting documents.", task.getException());
